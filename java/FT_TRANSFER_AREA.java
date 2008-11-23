c4b5feba6f8cdbe5831baed78a49f97dc173b636
zeuz <zeuz@1511ef63-6773-40bc-858c-0fa011d34c6e>
2008-11-23 14:12:25 +0000

    finished fields declaration, probably most of them should be declared as final

--- java/FT_TRANSFER_AREA.java
+++ java/FT_TRANSFER_AREA.java
@@ -35,69 +35,66 @@ public class FT_TRANSFER_AREA extends Structure {
 	public volatile byte res_bT;
 	public volatile byte[] resvd4 = new byte[1];
 	public volatile byte M_Main;
-	
-	/*
-	("M_Sub1", c_ubyte),
-	("M_Sub2", c_ubyte),
-	("M_Sub3", c_ubyte),
-	("rsvd5", c_ubyte*4),
-	("MES_Main", c_ubyte),
-	("MES_Sub1", c_ubyte),
-	("MES_Sub2", c_ubyte),
-	("MES_Sub3", c_ubyte),
-	("rsvd6", c_ubyte*1),
-	("reserverd_l1", c_ubyte),
-	("reserverd_l2", c_ubyte),
-	("reserverd_l3", c_ubyte),
-	("MPWM_Main", c_ubyte*8),
-	("MPWM_Sub1", c_ubyte*8),
-	("MPWM_Sub2", c_ubyte*8),
-	("MPWM_Sub3", c_ubyte*8),
-	("rsvd7", c_ubyte*32),
-	("A1S1", c_ushort),
-	("A1S2", c_ushort),
-	("A1S3", c_ushort),
-	("AVS1", c_ushort),
-	("AVS2", c_ushort),
-	("AVS3", c_ushort),
-	("rsvd8", c_ubyte*4),
-	("AX_R", c_ushort),
-	("AY_R", c_ushort),
-	("AXS1_R", c_ushort),
-	("AXS2_R", c_ushort),
-	("AXS3_R", c_ushort),
-	("rsvd10", c_ubyte*54),
-	("Mode", c_ubyte),
-	("MPWM_Update", c_ubyte),
-	("TransferAktiv", c_ubyte),
-	("rsvd11", c_ubyte*3),
-	("BudModules", c_ubyte),
-	("SlotModule1", c_ubyte),
-	("SlotModule2", c_ubyte),
-	("rsvd12", c_ubyte*7),
-	("ChangeEg", c_ubyte),
-	("ChangeAn", c_ubyte),
-	("ChangeIr", c_ubyte),
-	("rsvd13", c_ubyte*13),
-	("E16_Main", c_ubyte*8),
-	("E16_Sub1", c_ubyte*8),
-	("E16_Sub2", c_ubyte*8),
-	("E16_Sub3", c_ubyte*8),
-	("Distance16", c_ushort*2),
-	("rsvd14", c_ubyte*12),
-	("IRKeys16", c_ushort*11),
-	("rsvd15", c_ubyte*15),
-	("IRKeys16Code1", c_ushort*11),
-	("rsvd16", c_ubyte*10),
-	("IRKeys16Code2", c_ushort*11),
-	("rsvd17", c_ubyte*10),
-	("RfStatus", c_ushort),
-	("RfPower", c_ushort),
-	("RfError", c_ushort),
-	("MsgNumInBuffer", c_ushort),
-	("RfModulNr", c_ushort),
-	("rsvd19", c_ubyte*2),
-	("dwTc", c_long),
-	("rsvd20", c_ubyte*64),
-	*/
+	public volatile byte M_Sub1;
+	public volatile byte M_Sub2;
+	public volatile byte M_Sub3;
+	public volatile byte[] rsvd5 = new byte[4];
+	public volatile byte MES_Main;
+	public volatile byte MES_Sub1;
+	public volatile byte MES_Sub2;
+	public volatile byte MES_Sub3;
+	public volatile byte[] rsvd6 = new byte[1];
+	public volatile byte reserverd_l1;
+	public volatile byte reserverd_l2;
+	public volatile byte reserverd_l3;
+	public volatile byte[] MPWM_Main = new byte[8];
+	public volatile byte[] MPWM_Sub1 = new byte[8];
+	public volatile byte[] MPWM_Sub2 = new byte[8];
+	public volatile byte[] MPWM_Sub3 = new byte[8];
+	public volatile byte[] rsvd7 = new byte[32];
+	public volatile short A1S1;
+	public volatile short A1S2;
+	public volatile short A1S3;
+	public volatile short AVS1;
+	public volatile short AVS2;
+	public volatile short AVS3;
+	public volatile byte[] rsvd8 = new byte[4];
+	public volatile short AX_R;
+	public volatile short AY_R;
+	public volatile short AXS1_R;
+	public volatile short AXS2_R;
+	public volatile short AXS3_R;
+	public volatile byte rsvd10 = new byte[54];
+	public volatile byte Mode;
+	public volatile byte MPWM_Update;
+	public volatile byte TransferAktiv;
+	public volatile byte[] rsvd11 = new byte[3];
+	public volatile byte BudModules;
+	public volatile byte SlotModule1;
+	public volatile byte SlotModule2;
+	public volatile byte[] rsvd12 = new byte[7];
+	public volatile byte ChangeEg;
+	public volatile byte ChangeAn;
+	public volatile byte ChangeIr;
+	public volatile byte[] rsvd13 = new byte[13];
+	public volatile byte[] E16_Main = new byte[8];
+	public volatile byte[] E16_Sub1 = new byte[8];
+	public volatile byte[] E16_Sub2 = new byte[8];
+	public volatile byte[] E16_Sub3 = new byte[8];
+	public volatile short[] Distance16 = new short[2];
+	public volatile byte[] rsvd14 = new byte[12];
+	public volatile short[] IRKeys16 = new short[11];
+	public volatile byte rsvd15 = new byte[15];
+	public volatile short[] IRKeys16Code1 = new short[11];
+	public volatile byte rsvd16 = new byte[10];
+	public volatile short[] IRKeys16Code2 = new short[11];
+	public volatile byte[] rsvd17 = new byte[10];
+	public volatile short RfStatus;
+	public volatile short RfPower;
+	public volatile short RfError;
+	public volatile short MsgNumInBuffer;
+	public volatile short RfModulNr;
+	public volatile byte[] rsvd19 = new byte[2];
+	public volatile NativeLong dwTc;
+	public volatile byte[] rsvd20 = new byte[64];
 }
