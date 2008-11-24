78925c66bc68a51704cb3606c7bcad51128b3522
zeuz <zeuz@1511ef63-6773-40bc-858c-0fa011d34c6e>
2008-11-24 15:16:44 +0000

    test now works, but needs readFile/writeFile method calls

--- java/test.java
+++ java/test.java
@@ -8,21 +8,34 @@ public class test {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Begin");
 		FTLibrary lib = FTLibrary.INSTANCE;
+	
+		// FT_HANDLE handle = lib.OpenFtCommDevice("/dev/ttyS3", 20, 10);
 		
-		lib.InitFtUsbDeviceList();
+ 		lib.InitFtUsbDeviceList();
 		FT_HANDLE handle = lib.GetFtUsbDeviceHandle((new Integer(0)).byteValue());
 		lib.OpenFtUsbDevice(handle);
 		
 		lib.StartFtTransferArea(handle, 0);
 		
 		FT_TRANSFER_AREA area = lib.GetFtTransferAreaAddress(handle);
-		area.M_Main = 1;
-		for(int i=0; i < 10; i++) {
-			area = lib.GetFtTransferAreaAddress(handle);
-			System.out.println(area.E_Main);
-			Thread.sleep(1000);
-		}
-		area.M_Main = 0;
+		System.out.println("Transfer Aktiv:" + area.TransferAktiv);
+		area.MPWM_Main[0] = 7; // PWM Motor 1
+		area.MPWM_Main[1] = 6;
+		area.MPWM_Main[2] = 7; // PWM Motor 2
+		area.writeField("MPWM_Main");
+		// area.M_Main = 1;
+		area.writeField("M_Main", (byte)1); // Motor 1 right
+		Thread.sleep(500);
+		// area.M_Main = 2;
+		area.writeField("M_Main", (byte)6); // Motor 1 & 2 left
+		do {
+			// area = lib.GetFtTransferAreaAddress(handle);
+			System.out.println(area.readField("E_Main"));
+			Thread.sleep(100);
+			area.readField("E_Main"); // read E_Main from Hardware
+		} while(area.E_Main != (byte)1); // stop when Digital Input 1 is shorted
+		// area.M_Main = 0;
+		area.writeField("M_Main", (byte)0);
 		
 		lib.StopFtTransferArea(handle);
 		lib.CloseFtDevice(handle);
