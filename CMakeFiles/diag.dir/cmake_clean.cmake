c65ca0e0e5ff07e00f185b6cb378e9030f68c552
erik <erik@1511ef63-6773-40bc-858c-0fa011d34c6e>
2011-11-30 18:57:06 +0000

    Version to 0.4.4

--- CMakeFiles/diag.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/diag"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/diag.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- CMakeFiles/python.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/python"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/python.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
