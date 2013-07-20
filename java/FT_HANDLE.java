1d871f9c441f7c8fa4902079a6352f6e514a895d
erik <erik@1511ef63-6773-40bc-858c-0fa011d34c6e>
2013-07-20 14:58:24 +0000

    applied java fixes by Andre Mohr

--- java/FT_HANDLE.java
+++ java/FT_HANDLE.java
@@ -3,4 +3,11 @@ import com.sun.jna.Structure;
 
 public class FT_HANDLE extends Structure {
 	public FT_TRANSFER_AREA transfer_area;
+	
+	@Override
+	protected List getFieldOrder() {
+		return Arrays.asList(new String[]{
+			"transfer_area"
+		});
+	}
 }
